33743b95dc4062d0ccd9d8d0e7524159a44c260b
watosar/nekojyarasi <38435692+watosar@users.noreply.github.com>
2020-09-29 17:31:41 +0900

    add pre made files

---
+++ ClientApp/src/client.cpp
@@ -0,0 +1,158 @@
+﻿// client.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
+//
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+#include <iostream>
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <conio.h>
+
+#include "../headers/ShareMem.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int main()
+{
+	WSADATA wsaData;
+	int iResult;
+
+	// Initialize Winsock
+	iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+	if (iResult != 0)
+	{
+		printf("WSAStartup failed: %d\n", iResult);
+		return 1;
+	}
+
+#define DEFAULT_PORT "27015"
+
+	struct addrinfo *result = NULL, *ptr = NULL, hints;
+
+	ZeroMemory(&hints, sizeof(hints));
+	hints.ai_family = AF_INET;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_protocol = IPPROTO_TCP;
+	hints.ai_flags = AI_PASSIVE;
+	// Resolve the local address and port to be used by the server
+	iResult = getaddrinfo(NULL, DEFAULT_PORT, &hints, &result);
+	if (iResult != 0)
+	{
+		printf("getaddrinfo failed: %d\n", iResult);
+		WSACleanup();
+		return 1;
+	}
+	SOCKET ListenSocket = INVALID_SOCKET;
+	// Create a SOCKET for the server to listen for client connections
+
+	ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+	if (ListenSocket == INVALID_SOCKET)
+	{
+		printf("Error at socket(): %ld\n", WSAGetLastError());
+		freeaddrinfo(result);
+		WSACleanup();
+		return 1;
+	}
+	// Setup the TCP listening socket
+	iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("bind failed with error: %d\n", WSAGetLastError());
+		freeaddrinfo(result);
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	freeaddrinfo(result);
+	printf("socket: port is 27015");
+	if (listen(ListenSocket, SOMAXCONN) == SOCKET_ERROR)
+	{
+		printf("Listen failed with error: %ld\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+	SOCKET ClientSocket;
+	ClientSocket = INVALID_SOCKET;
+
+	// Accept a client socket
+	ClientSocket = accept(ListenSocket, NULL, NULL);
+	if (ClientSocket == INVALID_SOCKET)
+	{
+		printf("accept failed: %d\n", WSAGetLastError());
+		closesocket(ListenSocket);
+		WSACleanup();
+		return 1;
+	}
+
+#define DEFAULT_BUFLEN 1024
+
+	char recvbuf[DEFAULT_BUFLEN];
+	int iSendResult;
+	int recvbuflen = DEFAULT_BUFLEN;
+
+	std::cout << "start\n";
+
+	SharedMemory comm("pipe");
+	if (!comm.is_open())
+	{
+		return -1;
+	}
+
+	char *SharedRam = (char *)comm.get_pointer();
+
+	SharedRam[0] = '\0';
+	while (true)
+	{
+		//WaitForWaitData(SharedRam); //データ待ちになるまで待つ
+
+		char recvbuf[DEFAULT_BUFLEN] = {};
+		iResult = recv(ClientSocket, recvbuf, recvbuflen, 0);
+		if (iResult > 0)
+		{
+			printf("Bytes received: %d,%d\n", iResult, (unsigned)strlen(recvbuf));
+			//recvbuf[iResult] = '\0';
+		}
+		else if (iResult == 0)
+		{
+			printf("Connection closing...\n");
+			break;
+		}
+		else
+		{
+			printf("recv failed: %d\n", WSAGetLastError());
+			closesocket(ClientSocket);
+			WSACleanup();
+			return 1;
+		}
+
+		if (SharedRam[0] == 'x')
+		{
+			comm.print(recvbuf);
+		}
+		printf("->%s\n", recvbuf);
+	}
+
+	// shutdown the connection since we're done
+	iResult = shutdown(ClientSocket, SD_SEND);
+	if (iResult == SOCKET_ERROR)
+	{
+		printf("shutdown failed with error: %d\n", WSAGetLastError());
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 1;
+	}
+
+	// cleanup
+	closesocket(ClientSocket);
+	WSACleanup();
+
+	return 0;
+}
